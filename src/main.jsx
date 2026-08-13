3984af776e2261afc68ba98a0d5d70efcb7aa24e
jihye21 <112855236+jihye21@users.noreply.github.com>
2026-08-13 05:34:59 +0000

    fix: 설정 수정

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/money_box">
       <Routes>
         {/* 로그인 페이지 경로 */}
         <Route path="/login" element={<LoginPage />} />
